75dec10476d300c4a8b7d6082280ebe0318af264
jojo-jie <18624275868@163.com>
2021-08-17 20:03:39 +0800

    struct{} 对齐补充

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -23,7 +23,7 @@ func TestOnEvicted(t *testing.T) {
 	is.Equal(expected, keys)
 }
 
-//unsafe.Sizeof 返回数据类型大小
+//unsafe.Sizeof 返回数据类型大小 空结构体大小==0
 //unsafe.Alignof 返回类型对齐系数
 //内存对其
 //对于结构体的各个成员，第一个成员位于偏移为0的位置，结构体第一个成员的偏移量(offset)为0，以后每个成员相对于结构体首地址的offset都是该成员大小与有效对齐值中较小那个的整数倍，如有需要编译器会在成员之间加上填充字节。
@@ -34,6 +34,7 @@ type Demo struct {
 	B []int32
 	C string
 	D bool
+	E struct{}
 }
 
 type test1 struct {
@@ -55,12 +56,24 @@ type test3 struct {
 	a int32  // 4 4
 }
 
+type test4 struct {
+	b []int32
+	a struct{} //struct{} 在最后会被填充对齐到前一个字段的对齐系数
+}
+
+func TestStruct(t *testing.T) {
+	t4 := test4{}
+	t.Log(unsafe.Alignof(t4.b))
+	t.Log(unsafe.Sizeof(t4))
+}
+
 func TestSize(t *testing.T) {
 	var s Demo
 	t.Log(unsafe.Alignof(s.A))
 	t.Log(unsafe.Sizeof(s.A))
 	t.Log(unsafe.Sizeof(s.B))
 	t.Log(unsafe.Sizeof(s.C))
+	t.Log(unsafe.Alignof(s.E))
 	// 0-3 4-7 8-31 32 47 48 56
 
 	// 0 1-3 4-7 8->23 => 24
